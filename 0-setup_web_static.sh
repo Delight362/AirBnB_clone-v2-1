66cb9bce2e43687bd850bbb08753e141689d4069
karlie moyo <karlieemoyo@gmail.com>
2023-04-06 13:36:37 +0200

    Sets up a web server for deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,9 +21,27 @@ echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\
 ln -sf /data/web_static/release/test/ /data/web_static/current
 
 # give ownership of the/data/ folder to the ubuntu user and group
-chown -R ubuntu:ubuntu /data
-line="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
-sed -i "60i $line" /etc/nginx/sites_enabled/default
+chown -R ubuntu:ubuntu /data/
+chgrp -R ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 # restart nginx to apply the changes
 service nginx restart
