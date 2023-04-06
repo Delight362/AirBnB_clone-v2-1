23ff9ebbba08f42d19d8911b30221f793288ec33
karlie moyo <karlieemoyo@gmail.com>
2023-04-06 21:07:45 +0200

    Sets up web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,51 +1,18 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
-
-# Update servers repository and install nginx
-apt update
-apt -y install nginx
-
-# create project directories
-mkdir -p /data/web_static/shared/
+# sets up web servers for the deployment of web_static.
+apt update -y
+apt install nginx -y
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/test/
-
-# create initial html
-cat << EOF | tee /data/web_static/releases/test/index.html
-<html>
+mkdir -p /data/web_static/shared/
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-EOF
-
-# create a symlink to ..release/test/ dir
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# give ownership of /data/ to the current user
-chown -R ubuntu:ubuntu /data/
-
-# set up nginx server configuration
-SERVER=$(hostname)
-
-SERVER_CONFIG=\
-"server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        location / {
-                add_header X-Served-By '$SERVER';
-                try_files \$uri \$uri/ =404;
-        }
-	location /hbnb_static {
-                add_header X-Served-By '$SERVER';
-		alias /data/web_static/current;
-	}
-}"
-bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
-/etc/init.d/nginx restart
-
-exit 0
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
